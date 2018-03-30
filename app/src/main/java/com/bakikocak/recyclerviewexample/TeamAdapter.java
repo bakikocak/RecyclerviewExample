f060bd5db193b6f1fa992adea423161019d47869
Baki Kocak <abdulbakikocak@gmail.com>
2018-03-30 14:05:24 +0300

    Comments & explanation

--- app/src/main/java/com/bakikocak/recyclerviewexample/TeamAdapter.java
+++ app/src/main/java/com/bakikocak/recyclerviewexample/TeamAdapter.java
@@ -10,19 +10,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 import java.util.List;
 
-
+//Basic adapter creation extending from RecyclerView.Adapter and then specifying the custom ViewHolder called TeamViewHolder
 public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder> {
-
+    //Let's store context and a list of Team objects
     Context context;
     List<Team> teamList;
 
+    // Pass in the team list & context into the constructor
     public TeamAdapter(Context context, List<Team> teamList) {
         this.context = context;
         this.teamList = teamList;
     }
 
 
-    @NonNull
+    // Inflates custom layout for a RecyclerView item and returns the viewHolder
     @Override
     public TeamViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         Context context = parent.getContext();
@@ -34,6 +35,7 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder
         return viewHolder;
     }
 
+    // Populates data into the item through holder
     @Override
     public void onBindViewHolder(@NonNull TeamViewHolder holder, int position) {
         holder.teamTitleTextView.setText(teamList.get(position).getTitle());
@@ -41,13 +43,15 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder
         holder.yearTextView.setText(teamList.get(position).getYear());
     }
 
+    // Returns the total count of items in the list
     @Override
     public int getItemCount() {
         return teamList.size();
     }
 
+    //ViewHolder that gives us direct access to each view.
     class TeamViewHolder extends RecyclerView.ViewHolder{
-
+        //Member views accesed via ViewHolder
         TextView teamTitleTextView;
         TextView leagueNameTextView;
         TextView yearTextView;
@@ -60,6 +64,7 @@ public class TeamAdapter extends RecyclerView.Adapter<TeamAdapter.TeamViewHolder
             leagueNameTextView = (TextView) itemView.findViewById(R.id.tv_league_name);
             yearTextView = (TextView) itemView.findViewById(R.id.tv_year_text);
 
+            //OnClickListener triggered when a view clicked on RecyclerView
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
